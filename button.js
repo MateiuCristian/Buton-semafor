68beecff3c4604fc1cfda1b296e19515f311fcd6
Mateiu Cristian <102381148+MateiuCristian@users.noreply.github.com>
2025-11-19 09:26:25 +0200

    Update and rename buttonScript.js to button.js

---
+++ button.js
@@ -0,0 +1,12 @@
+colors = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 128, 0)"];
+i = 0;
+
+function changeButtonColor(id) {
+    document.getElementById(id).style.backgroundColor = colors[(i + 1) % 3];
+    console.log(colors[i]);
+    ++i;
+}
+
+function startTimer(id) {
+    setInterval(changeButtonColor, 5000, id)
+}
